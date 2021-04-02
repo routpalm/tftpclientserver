26822a73d89986c800c922e8148b6b8e7b62ebe6
nichilus <nichilusa@gmail.com>
2021-04-02 12:13:07 -0700

    added tftpconstants 4/2/2021

---
+++ TFTPConstants.java
@@ -0,0 +1,31 @@
+public interface TFTPConstants {
+   public static final int RRQ = 1;
+   
+   public static final int WRQ = 2;
+   
+   public static final int DATA = 3;
+   
+   public static final int ACK = 4;
+   
+   public static final int ERROR = 5;
+   
+   public static final int UNDEF = 0;
+   
+   public static final int NOTFND = 1;
+   
+   public static final int ACCESS = 2;
+   
+   public static final int DSKFUL = 3;
+   
+   public static final int ILLOP = 4;
+   
+   public static final int UNKID = 5;
+   
+   public static final int FILEX = 6;
+   
+   public static final int NOUSR = 7;
+   
+   public static final int MAX_PACKET_SIZE = 1500;
+   
+   public static final int TFTP_PORT = 69;
+}
